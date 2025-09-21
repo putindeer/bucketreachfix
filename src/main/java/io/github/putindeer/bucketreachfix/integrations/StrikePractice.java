3b885ddb9afc6118bdcf4b2a6995975af86fc204
Diego <putindeergaming@gmail.com>
2025-09-21 04:54:20 -0300

    Revert "Update StrikePractice.java"
    This reverts commit a9a809db7ae972fdf2786dcf3d7966979750d878.

--- src/main/java/io/github/putindeer/bucketreachfix/integrations/StrikePractice.java
+++ src/main/java/io/github/putindeer/bucketreachfix/integrations/StrikePractice.java
@@ -4,7 +4,7 @@ import io.github.putindeer.bucketreachfix.Main;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.entity.Player;
-import ga.strikepractice.event.FightEndEvent;
+import io.github.strikepractice.api.events.FightEndEvent;
 
 public class StrikePractice implements Listener {
     private final Main plugin;
